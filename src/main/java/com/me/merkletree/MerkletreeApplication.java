980415dad607d9e59bb08b3bc760e07d1577fdb1
chickenchickenlove <ojt90902@naver.com>
2023-08-20 15:07:57 +0900

    git init

---
+++ src/main/java/com/me/merkletree/MerkletreeApplication.java
@@ -0,0 +1,13 @@
+package com.me.merkletree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerkletreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerkletreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/me/merkletree/MerkletreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.merkletree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerkletreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
